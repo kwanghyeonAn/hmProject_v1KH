52cd9dffbabd9683ef71fab299b0cdf44160296e
Ankwanghyeon <Ankwanghyeon@DESKTOP-M1BCVUK>
2020-09-03 15:12:57 +0900

    DTO완료

---
+++ src/DTO/item_DTO.java
@@ -0,0 +1,36 @@
+package DTO;
+
+public class item_DTO {
+
+	int itemNo;
+	String itemName;
+	int itemPrice;
+	int itemCnt;
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getItemCnt() {
+		return itemCnt;
+	}
+	public void setItemCnt(int itemCnt) {
+		this.itemCnt = itemCnt;
+	}
+	
+	
+	
+}
---
+++ src/DTO/member_DTO.java
@@ -0,0 +1,50 @@
+package DTO;
+
+public class member_DTO {
+
+	int no;
+	String id;
+	String pwd;
+	String name;
+	String gender;
+	int age;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
